ee20996ed76258d4d42ed4d6fe804bbc836da9f4
Ammar Askar <ammar@ammaraskar.com>
2018-07-02 01:39:49 -0400

    Add script to run benchmarks

---
+++ scripts/runBenchmark.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+import os
+from pathlib import Path
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: runBenchmark.py <file> <time>")
+        sys.exit(1)
+
+    path = Path(sys.argv[1])
+    file_name = path.name
+    print("[+] Benchmarking compile time for " + file_name)
+
+    command = ["stack", "exec", "--", 
+               "supercompile", "--time-limit", sys.argv[2], 
+               "--json", "statistics/" + file_name + ".compile.json"]
+    print("> " + (" ".join(command)))
+
+    subprocess.check_call(
+        command,
+        env=dict(os.environ, BENCHMARK_FILE=sys.argv[1])
+    )
+
+    # Compile the regular program
+    command = ["stack", "ghc", "--", "-O2", 
+               "-o", "statistics/" + file_name + ".compiled", 
+               str(Path("input") / path.parent / (path.stem + ".hs"))]
+    print("[+] Compiling input program")
+    print("> " + (" ".join(command)))
+
+    subprocess.check_call(command)
+
+    # Compile the supercompiled program
+    command = ["stack", "ghc", "--", "-O2", 
+               "-o", "statistics/" + file_name + ".supercompiled", 
+               str(Path("output") / path.parent / (path.stem + ".hs"))]
+    print("[+] Supercompiling input program")
+    print("> " + (" ".join(command)))
+
+    subprocess.check_call(command)
+
+    # Run the regular program
+    command = ["statistics/" + file_name + ".compiled",
+               "--time-limit", sys.argv[2],
+               "--json", "statistics/" + file_name + ".compiled.json"]
+    print("> " + (" ".join(command)))
+    subprocess.check_call(command)
+
+    # Run the supercompiled program
+    command = ["statistics/" + file_name + ".supercompiled",
+               "--time-limit", sys.argv[2],
+               "--json", "statistics/" + file_name + ".supercompiled.json"]
+    print("> " + (" ".join(command)))
+    subprocess.check_call(command)
+
+if __name__ == "__main__":
+    main()
---
+++ statistics/README.md
@@ -0,0 +1,4 @@
+# Benchmarking Output Lives Here
+
+This file exists so we can store this folder in git, run
+`scripts/runBenchmark.py` to actually get stuff in here.
